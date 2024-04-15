0234780a21734b3a6fcad03c6b8176edfbe84e00
Adnan Yusuf <dnnyusuf@gmail.com>
2024-04-15 16:52:20 +0100

    testing 4.0

--- app.js
+++ app.js
@@ -32,7 +32,12 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch((err) => console.log('Database connection error:', err));
 
 // routes
-app.get('*', checkUser);
+// Specific routes first
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes);
+
+// Then the wildcard route
+app.get('*', checkUser);
+
 app.use(authRoutes);
