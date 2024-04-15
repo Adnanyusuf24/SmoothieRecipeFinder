fe1c0086bff9444373eb2836f6605803c5f9c3cd
Adnan Yusuf <dnnyusuf@gmail.com>
2024-04-15 19:45:01 +0100

    testing 9.0

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const authRoutes = require('../routes/authRoutes');
 const cookieParser = require('cookie-parser');
 const { requireAuth, checkUser } = require('../middleware/authMiddleware');
-
+const path = require('path');
 const app = express();
 
 // middleware
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // view engine
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // database connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
